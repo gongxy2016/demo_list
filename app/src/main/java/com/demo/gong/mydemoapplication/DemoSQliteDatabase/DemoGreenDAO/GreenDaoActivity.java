d23a2ae366389f2ff77860e0aad520dddbeb6f88
TS_02 <gong_xyu@163.com>
2018-12-06 17:13:52 +0800

    update:DemoGreenDao

--- app/src/main/java/com/demo/gong/mydemoapplication/DemoSQliteDatabase/DemoGreenDAO/GreenDaoActivity.java
+++ app/src/main/java/com/demo/gong/mydemoapplication/DemoSQliteDatabase/DemoGreenDAO/GreenDaoActivity.java
@@ -3,6 +3,7 @@ package com.demo.gong.mydemoapplication.DemoSQliteDatabase.DemoGreenDAO;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.demo.gong.mydemoapplication.DemoSQliteDatabase.DemoGreenDAO.dao.DaoSession;
 import com.demo.gong.mydemoapplication.MyApplication;
 import com.demo.gong.mydemoapplication.R;
 
@@ -14,8 +15,9 @@ public class GreenDaoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_green_dao);
-
-        sqLiteUtils = SQLiteUtils.getInstance(MyApplication.getInstance().getDaoSession().getPersonBeanDao(),MyApplication.getInstance().getDaoSession());
+        MyApplication mApplication = MyApplication.getInstance();
+        DaoSession daoSession = mApplication.getDaoSession();
+        sqLiteUtils = SQLiteUtils.getInstance(daoSession.getPersonBeanDao(),mApplication.getDaoSession());
         sqLiteUtils.insert(new PersonBean( 1L,"lf","131648","001","iphone"));
         sqLiteUtils.insert(new PersonBean(2L,"pg","131649","002","iphone"));
         sqLiteUtils.insert(new PersonBean(3L,"nm","131650","003","oppo"));
