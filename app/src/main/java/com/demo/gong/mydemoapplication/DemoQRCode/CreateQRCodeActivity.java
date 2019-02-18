3a8d32b2b864d6922b90aaa59adb7c64e84a2046
TS_02 <gong_xyu@163.com>
2019-02-18 15:54:06 +0800

    update:ScanQRCode

---
+++ app/src/main/java/com/demo/gong/mydemoapplication/DemoQRCode/CreateQRCodeActivity.java
@@ -0,0 +1,26 @@
+package com.demo.gong.mydemoapplication.DemoQRCode;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.demo.gong.mydemoapplication.BaseActivity;
+import com.demo.gong.mydemoapplication.R;
+
+import butterknife.BindView;
+
+public class CreateQRCodeActivity extends BaseActivity {
+
+    @BindView(R.id.iv_qrcode)
+    ImageView ivQRCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_qrcode);
+
+        Bitmap mBitmap = QRCodeUtil.createQRCodeBitmap("https://www.baidu.com", 480, 480);
+        ivQRCode.setImageBitmap(mBitmap);
+    }
+}
--- app/src/main/java/com/demo/gong/mydemoapplication/DemoQRCode/QRCodeActivity.java
+++ app/src/main/java/com/demo/gong/mydemoapplication/DemoQRCode/QRCodeActivity.java
@@ -1,27 +1,80 @@
 package com.demo.gong.mydemoapplication.DemoQRCode;
 
-import android.graphics.Bitmap;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
 import com.demo.gong.mydemoapplication.BaseActivity;
 import com.demo.gong.mydemoapplication.R;
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.bean.ZxingConfig;
+import com.yzq.zxinglibrary.common.Constant;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class QRCodeActivity extends BaseActivity {
 
-    @BindView(R.id.iv_qrcode)
-    ImageView ivQRCode;
+    @BindView(R.id.btn_create_qrcode)
+    Button btnCreateQRCode;
+
+    @BindView(R.id.btn_scan_qrcode)
+    Button btnScanQRCode;
+
+    @OnClick({R.id.btn_create_qrcode,R.id.btn_scan_qrcode})
+    public void onViewClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_create_qrcode :
+                startActivity(new Intent(this,CreateQRCodeActivity.class));
+                break;
+
+            case R.id.btn_scan_qrcode :
+                Intent intent = new Intent(this, CaptureActivity.class);
+                /*ZxingConfig是配置类
+                 *可以设置是否显示底部布局，闪光灯，相册，
+                 * 是否播放提示音  震动
+                 * 设置扫描框颜色等
+                 * 也可以不传这个参数
+                 * */
+                ZxingConfig config = new ZxingConfig();
+                config.setPlayBeep(true);//是否播放扫描声音 默认为true
+                config.setShake(true);//是否震动  默认为true
+                config.setDecodeBarCode(true);//是否扫描条形码 默认为true
+                config.setReactColor(R.color.colorAccent);//设置扫描框四个角的颜色 默认为白色
+                config.setFrameLineColor(R.color.colorAccent);//设置扫描框边框颜色 默认无色
+                config.setScanLineColor(R.color.colorAccent);//设置扫描线的颜色 默认白色
+                config.setFullScreenScan(false);//是否全屏扫描  默认为true  设为false则只会在扫描框中扫描
+                intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
+                startActivityForResult(intent, 0);
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
 
-        Bitmap mBitmap = QRCodeUtil.createQRCodeBitmap("https://www.baidu.com", 480, 480);
-        ivQRCode.setImageBitmap(mBitmap);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 扫描二维码/条码回传
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            if (data != null) {
+
+                String content = data.getStringExtra(Constant.CODED_CONTENT);
+                Toast.makeText(this, "扫描结果：" + content, Toast.LENGTH_SHORT).show();
+                Log.i("QRCodeActivity","扫描结果为：" + content);
+
+            }
+        }
     }
 }
