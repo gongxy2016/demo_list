baace75f134b9c9723b0eb18b017432a5adb43e4
TS_02 <gong_xyu@163.com>
2019-08-27 08:46:10 +0800

    update

---
+++ app/src/main/java/com/demo/gong/mydemoapplication/DemoRxjava/RxjavaPractiseActivity.java
@@ -0,0 +1,17 @@
+package com.demo.gong.mydemoapplication.DemoRxjava;
+
+import android.os.Bundle;
+
+import com.demo.gong.mydemoapplication.BaseActivity;
+import com.demo.gong.mydemoapplication.R;
+
+public class RxjavaPractiseActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rxjava_practise);
+
+
+    }
+}
